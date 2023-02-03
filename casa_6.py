b572bd369b391b425c350e9e1662f5e38f968e51
Eduardo (David) Villacis Calderon <davicompu@users.noreply.github.com>
2023-02-03 12:28:49 -0500

    Update casa_6.py

--- casa_6.py
+++ casa_6.py
@@ -1,10 +1,10 @@
 # Do not forget to change values for the variables fruit and s
 # Do not forget to comment out the lines of code that will trigger Type and Index errors.
 
-# Chapter 6.1 "A string is a sequence"
+# Chapter 6.1 "A string is a sequence" code
 
-# Chapter 6.2 "Getting the length of a string using len"
+# Chapter 6.2 "Getting the length of a string using len" code
 
-# Chapter 6.3 "Traverse through a string with a loop" 
+# Chapter 6.3 "Traverse through a string with a loop" code
 
-# Chapter 6.4 "String Slices" 
+# Chapter 6.4 "String Slices" code
