ff1ceeff2b40d3b97289a175cc57c02db6986ebe
Eduardo (David) Villacis Calderon <davicompu@users.noreply.github.com>
2023-02-03 12:27:24 -0500

    Creating casa_6.py and leaving comments for students

---
+++ casa_6.py
@@ -0,0 +1,7 @@
+# Chapter 6.1 "A string is a sequence"
+
+# Chapter 6.2 "Getting the length of a string using len"
+
+# Chapter 6.3 "Traverse through a string with a loop" 
+
+# Chapter 6.4 "String Slices" 
